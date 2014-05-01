6f12864d37df3818d32f89752f6516a8c766870f
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-05-01 15:17:34 +0530

    Initial commit

---
+++ src/superdosna/SuperDosna.java
@@ -0,0 +1,20 @@
+package superdosna;
+
+/**
+ * Main class of SuperDosna implementation.
+ *
+ * @author Joshua Kissoon
+ * @since 20140501
+ */
+public class SuperDosna
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+
+}
