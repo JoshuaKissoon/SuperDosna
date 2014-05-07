cd4b50019d2467f30c460724e0387f7689d5e262
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-05-07 20:11:15 +0530

    Updating to DOSNA - Updating the SuperDOSNA to the updated DOSNA - FINISHED!!!

---
+++ src/dosna/simulations/performance/SimulatedUserActions.java
@@ -0,0 +1,180 @@
+package dosna.simulations.performance;
+
+import dosna.osn.actor.Actor;
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * This is a class that simulates the user activities for a single user;
+ * this is part of the overall simulation of users.
+ *
+ * @author Joshua Kissoon
+ * @since 20140505
+ */
+public class SimulatedUserActions implements Runnable
+{
+
+    private final SimConfig config;
+    private int numContent, numConnections, numActivityStreamRefreshes, numContentModified;
+    private final SimulatedUser simulatedUser;
+    private final CountDownLatch threadsWaiter;
+
+    
+    {
+        this.numContent = 0;
+        this.numConnections = 0;
+        this.numActivityStreamRefreshes = 0;
+        this.numContentModified = 0;
+    }
+
+    /**
+     * Setup the simulation for a single user
+     *
+     * @param config        The simulation configuration
+     * @param simulatedUser The user in the simulation
+     * @param threadsWaiter A latch used to indicate to the main program when this thread is finished
+     */
+    public SimulatedUserActions(final SimulatedUser simulatedUser, SimConfig config, CountDownLatch threadsWaiter)
+    {
+        this.config = config;
+        this.simulatedUser = simulatedUser;
+        this.threadsWaiter = threadsWaiter;
+    }
+
+    @Override
+    public void run()
+    {
+        while (numContent < config.numContent() || numConnections < config.numConnections()
+                || numActivityStreamRefreshes < config.numActivityStreamRefreshes() || numContentModified < config.numContentModifications())
+        {
+            try
+            {
+                if (numContent < config.numContent())
+                {
+                    /* Create and post a content */
+                    this.createAndPostContent();
+                    Thread.sleep(this.randomWaitPeriod());
+                }
+
+                if (numConnections < config.numConnections())
+                {
+                    /* Create a new connection */
+                    this.createConnection();
+                    Thread.sleep(this.randomWaitPeriod());
+                }
+
+                if (numActivityStreamRefreshes < config.numActivityStreamRefreshes())
+                {
+                    /* Refresh our activity stream here */
+                    this.refreshActivityStream();
+                    Thread.sleep(this.randomWaitPeriod());
+                }
+
+                if (numContentModified < config.numContentModifications())
+                {
+                    /* Modify some content here */
+                    this.modifyContent();
+                    Thread.sleep(this.randomWaitPeriod());
+                }
+
+            }
+            catch (InterruptedException ex)
+            {
+                System.err.println("User Simulation interrupted. ");
+            }
+        }
+
+        try
+        {
+            /* Wait a few seconds before shutting down */
+            Thread.sleep(5000);
+
+            /* Lets shutdown everything */
+            this.simulatedUser.stopKadRefreshOperation();
+            //System.out.println("Finished Executing everything....");
+
+            /* Finished executing!, lets inform the main program */
+            this.threadsWaiter.countDown();
+        }
+        catch (InterruptedException ex)
+        {
+
+        }
+    }
+
+    /**
+     * Handles creating and posting a new content on the DHT.
+     */
+    public synchronized void createAndPostContent()
+    {
+        try
+        {
+            String status = this.simulatedUser.getActor().getName() + " - Status " + numContent + " " + config.randomString();
+            this.simulatedUser.setNewStatus(status);
+            numContent++;
+        }
+        catch (IOException ex)
+        {
+            System.err.println("User " + this.simulatedUser.getActor().getId() + " error whiles posting status. Msg: " + ex.getMessage());
+        }
+    }
+
+    /**
+     * Handles creating a new connection.
+     */
+    public synchronized void createConnection()
+    {
+        int connectionUid = (this.simulatedUser.userNumber + numConnections + 1) % this.config.numUsers();
+        this.simulatedUser.createConnection(new Actor("" + connectionUid));
+        numConnections++;
+    }
+
+    /**
+     * Refreshes the activity stream
+     */
+    public synchronized void refreshActivityStream()
+    {
+        try
+        {
+            int numItems = this.simulatedUser.refreshActivityStream();
+            numActivityStreamRefreshes++;
+            if (numActivityStreamRefreshes == this.config.numActivityStreamRefreshes())
+            {
+                System.out.println(this.simulatedUser.getActor().getId() + " - Activity Stream Refreshed: " + numItems + " Content in activity stream");
+            }
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException ex)
+        {
+            System.err.println("Activity stream refresh thread interrupted exception. ");
+        }
+    }
+
+    /**
+     * Modify some content from a connection
+     */
+    public synchronized void modifyContent()
+    {
+        this.simulatedUser.updateRandomContent();
+        numContentModified++;
+    }
+
+    /**
+     * Computes a random wait period between the min and max wait period.
+     *
+     * @return The computed value
+     */
+    public synchronized long randomWaitPeriod()
+    {
+        /* Compute a random value */
+        long val = (long) (Math.random() * config.maxWaitPeriod());
+
+        /* If the value is less than the minimum wait period, we add the minimum wait to the value */
+        return (val < config.minWaitPeriod()) ? val + config.minWaitPeriod() : val;
+    }
+
+    public SimulatedUser getSimulatedUser()
+    {
+        return this.simulatedUser;
+    }
+}
---
+++ src/dosna/simulations/performance/Simulation.java
@@ -0,0 +1,155 @@
+package dosna.simulations.performance;
+
+import dosna.core.DOSNAStatistician;
+import java.util.concurrent.CountDownLatch;
+import kademlia.Statistician;
+
+/**
+ * The class that launches the simulation and aggregates the statistics after the completion of the simulation.
+ *
+ * @author Joshua Kissoon
+ * @since 20140505
+ */
+public class Simulation
+{
+
+    private final SimConfig config;
+    private final SimulatedUser[] users;
+
+    /* A count down latch to wait for all threads to execute */
+    private CountDownLatch threadsWaiter;
+
+    public Simulation()
+    {
+        this.config = new SimConfig();
+        this.users = new SimulatedUser[config.numUsers()];
+    }
+
+    /**
+     * Start the simulation
+     */
+    public void start()
+    {
+        /* INITIALIZE SIMULATED USERS & CONNECT THEM TO THE NETWORK */
+        threadsWaiter = new CountDownLatch(this.config.numUsers());
+        for (int i = 0; i < config.numUsers(); i++)
+        {
+            String actorId = "" + i;
+            this.users[i] = new SimulatedUser(actorId, actorId + "pass", actorId + " Name", i);
+            new Thread(new SimulatedUserInitialization(this.users[i], config, threadsWaiter)).start();
+            try
+            {
+                Thread.sleep(config.userCreationDelay());
+            }
+            catch (InterruptedException ex)
+            {
+
+            }
+        }
+
+        /* Wait for all threads to finish */
+        try
+        {
+            threadsWaiter.await();
+        }
+        catch (InterruptedException ex)
+        {
+
+        }
+
+        System.out.println("\nNodes Startup Finished. \n");
+
+        /* INITIALIZE THE USER'S CONTENT */
+        threadsWaiter = new CountDownLatch(this.config.numUsers());
+        for (int i = 0; i < config.numUsers(); i++)
+        {
+            /* Start a new thread for this user */
+            new Thread(new SimulatedUserCreateInitialContent(this.users[i], config, threadsWaiter)).start();
+        }
+
+        /* Wait for threads to finish */
+        try
+        {
+            threadsWaiter.await();
+        }
+        catch (InterruptedException ex)
+        {
+
+        }
+
+        System.out.println("\nInitial content creation finished. \n");
+
+        /* INITIALIZE THE USER'S CONNECTIONS */
+        threadsWaiter = new CountDownLatch(this.config.numUsers());
+        for (int i = 0; i < config.numUsers(); i++)
+        {
+            new Thread(new SimulatedUserCreateInitialConnections(this.users[i], config, threadsWaiter)).start();
+        }
+
+        /* Wait for threads to finish */
+        try
+        {
+            threadsWaiter.await();
+        }
+        catch (InterruptedException ex)
+        {
+
+        }
+
+        /* @todo Get the updated actor objects for the different simualated users */
+        System.out.println("\nInitial connections creation finished. \n");
+
+        try
+        {
+            Thread.sleep(30000);
+        }
+        catch (InterruptedException ex)
+        {
+
+        }
+
+        System.out.println("\nStarting the real time operations now. \n");
+
+        /* NOW LETS RUN THE PROCESSES */
+        threadsWaiter = new CountDownLatch(this.config.numUsers());
+        for (int i = 0; i < config.numUsers(); i++)
+        {
+            new Thread(new SimulatedUserActions(this.users[i], config, threadsWaiter)).start();
+        }
+
+        /* Wait for threads to finish */
+        try
+        {
+            threadsWaiter.await();
+        }
+        catch (InterruptedException ex)
+        {
+
+        }
+
+        /* PRINT USER'S DATA USAGE */
+        for (int i = 0; i < config.numUsers(); i++)
+        {
+            SimulatedUser simUser = this.users[i];
+            Statistician statsMan = simUser.getKademliaNode().getStatistician();
+            DOSNAStatistician dosnaStatsMan = simUser.getStatistician();
+
+            String stats = "User " + i + "; ";
+            stats += "Data Sent: " + statsMan.getTotalDataSent() + "; ";
+            stats += "Data Received: " + statsMan.getTotalDataReceived() + "; \n";
+            stats += "Bootstrap Time: " + statsMan.getBootstrapTime() + "; ";
+            stats += "# Content Lookups: " + statsMan.numContentLookups() + "; ";
+            stats += "Avg Content Lookup Time: " + statsMan.averageContentLookupTime() + "; \n";
+            stats += "Avg Content Lookup Route Length: " + statsMan.averageContentLookupRouteLength() + "; ";
+            stats += "Avg Activity Stream load time: " + dosnaStatsMan.avgActivityStreamLoadTime() + "; ";
+            stats += "\n";
+            System.out.println(stats);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Simulation sim = new Simulation();
+        sim.start();
+    }
+}
