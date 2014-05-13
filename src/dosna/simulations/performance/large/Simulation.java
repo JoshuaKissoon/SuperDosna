39f5a0f8dac9ed14c9b32abe374e33efe9a26842
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-05-13 20:45:01 +0530

    Setup our activity stream to check loading time as the time it takes to load content and not the time it takes for unknown content to load

--- src/dosna/simulations/performance/large/Simulation.java
+++ src/dosna/simulations/performance/large/Simulation.java
@@ -24,7 +24,7 @@ public class Simulation
 
     public Simulation()
     {
-        this.config = new SimConfig50Online();
+        this.config = new SimConfig();
         this.users = new SimulatedUser[config.numUsers()];
     }
 
