e2092c4452fd37bb251eb65145f42ba030311804
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-06-11 12:48:25 +0530

    Added a dosna.action package to contain all action classes
    Added the core Action interface to be inherited by all other actions

---
+++ src/dosna/action/Action.java
@@ -0,0 +1,12 @@
+package dosna.action;
+
+/**
+ * General interface for any action performed in the system
+ *
+ * @author Joshua Kissoon
+ * @since 20140611
+ */
+public interface Action
+{
+    
+}
