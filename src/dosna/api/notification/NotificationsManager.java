0181cb5751a4e9856ce906000f7c2cfb61f3b4af
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-06-11 15:59:50 +0530

    Added Blank methods to the notification manager class

--- src/dosna/api/notification/NotificationsManager.java
+++ src/dosna/api/notification/NotificationsManager.java
@@ -1,5 +1,7 @@
 package dosna.api.notification;
 
+import dosna.content.DOSNAContent;
+
 /**
  * Class that provides methods to handle sending notifications
  *
@@ -9,4 +11,30 @@ package dosna.api.notification;
 public class NotificationsManager
 {
 
+    /**
+     * Send a notification to a specific actor
+     *
+     * @param actorId The actor to send the notification to
+     * @param content The content to notify about
+     * @param message The notification message
+     *
+     * @return Whether the notification was successful or not
+     */
+    public boolean sendNotification(String actorId, DOSNAContent content, String message)
+    {
+
+    }
+
+    /**
+     * Send a notification to all actors related to a content
+     *
+     * @param content The content to notify about
+     * @param message The notification message
+     *
+     * @return Whether the notification was successful or not
+     */
+    public boolean sendNotification(DOSNAContent content, String message)
+    {
+
+    }
 }
