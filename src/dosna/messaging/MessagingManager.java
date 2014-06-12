6ae58ed952ae5b757228f144014c752bbae89120
Joshua Kissoon <JoshuaKissoon@gmail.com>
2014-06-12 10:49:49 +0530

    MessagingManager - Added methods to load a connection's MessageBox and to send a message

--- src/dosna/messaging/MessagingManager.java
+++ src/dosna/messaging/MessagingManager.java
@@ -27,7 +27,8 @@ public class MessagingManager
     }
 
     /**
-     * Load the MessageBox between the currently logged in actor and the specified connection from the DHT
+     * Load the MessageBox owned by the current actor
+     * - Containing messages between the currently logged in actor and the specified connection from the DHT
      *
      * @param connectionAid The ActorId of the connection with whom the MessageBox is shared
      *
@@ -50,5 +51,67 @@ public class MessagingManager
         {
             throw new ContentNotFoundException(ex.getMessage());
         }
+    }
+
+    /**
+     * Load the MessageBox owned by the specified connection
+     * - Containing messages between the currently logged in actor and the specified connection
+     *
+     * @param connectionAid The ActorId of the connection
+     *
+     * @return The connection's MessageBox
+     *
+     * @throws kademlia.exceptions.ContentNotFoundException
+     */
+    public MessageBox loadConnectionMessageBox(final String connectionAid) throws ContentNotFoundException
+    {
+        /* Create a temporary messagebox to get the DHT key of the required MessageBox */
+        MessageBox temp = new MessageBox(connectionAid, this.dosnaObjects.getActor().getId());
+
+        try
+        {
+            GetParameter gp = new GetParameter(temp.getKey(), temp.getType(), temp.getOwnerId());
+            MessageBox box = (MessageBox) temp.fromSerializedForm(this.dosnaObjects.getDataManager().get(gp).getContent());
+            return box;
+        }
+        catch (IOException | ContentNotFoundException ex)
+        {
+            throw new ContentNotFoundException(ex.getMessage());
+        }
+    }
+
+    /**
+     * Send a message from one user to another
+     *
+     * Since each Actor keeps a MessageBox between itself and it's connection, we'll have to update 2 MessageBoxes
+     *
+     * @param connnectionAid The ActorId of the connection to send this message to
+     * @param msg            The message to send
+     *
+     * @return The updated MessageBox of the current user
+     *
+     * @throws kademlia.exceptions.ContentNotFoundException
+     * @throws java.io.IOException
+     */
+    public MessageBox sendMessage(final Message msg, final String connnectionAid) throws ContentNotFoundException, IOException
+    {
+        /**
+         * Put the message in the actor->connection MessageBox
+         * - update this MessageBox on the DHT
+         */
+        MessageBox actorMb = this.loadMessageBox(connnectionAid);
+        actorMb.addMessage(msg);
+        this.dosnaObjects.getDataManager().put(actorMb);
+
+        /**
+         * Put the message in the connection->actor MessageBox
+         * - update this MessageBox on the DHT
+         */
+        MessageBox connectionMb = this.loadConnectionMessageBox(connnectionAid);
+        connectionMb.addMessage(msg);
+        this.dosnaObjects.getDataManager().put(connectionMb);
+
+        /* Return the updated messagebox of this actor */
+        return actorMb;
     }
 }
